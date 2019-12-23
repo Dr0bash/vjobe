8f368984ef77677524ff707cdc52def776604f57
Dr0bash <igor2-shamaev@yandex.ru>
2019-12-23 21:46:52 +0300

    javascript_pack_tag

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,3 @@
-/*const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require('@rails/webpacker')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment*/
-
-const { environment } = require('@rails/webpacker');
-const webpack = require('webpack');
-environment.plugins.append('Provide'
-    , new webpack.ProvidePlugin({
-        $: 'jquery'
-        ,
-        jQuery: 'jquery'
-        ,
-        Popper: ['popper.js',
-            'default']
-    }));
-module.exports = environment;
+module.exports = environment
