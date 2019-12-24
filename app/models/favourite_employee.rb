2c6380039ec251c3b952391a1ae6db89b821c323
Dr0bash <igor2-shamaev@yandex.ru>
2019-12-24 19:15:53 +0300

    added :

--- app/models/favourite_employee.rb
+++ app/models/favourite_employee.rb
@@ -1,5 +1,5 @@
 class FavouriteEmployee < ApplicationRecord
-  belongs_to :employer, foreign_key: employers_id
-  belongs_to :employee, foreign_key: employees_id
+  belongs_to :employer, foreign_key: :employers_id
+  belongs_to :employee, foreign_key: :employees_id
 end
 
